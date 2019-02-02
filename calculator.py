eda9e350511d0746a20aa182ac0909f533183e1f
Jai <lad.jai@gmail.com>
2019-02-02 14:22:23 -0500

    * Initial check-in

---
+++ calculator.py
@@ -0,0 +1,13 @@
+"""
+Calculator library containing basic math operations.
+
+This example has been sourced from : https://realpython.com/python-continuous-integration/
+"""
+
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+
+def subtract(first_term, second_term):
+    return first_term - second_term
